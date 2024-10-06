3ae99364b769c041c993cd582c2ad112833f5499
Sujoy Paul <freelancersujoy@gmail.com>
2024-10-07 01:17:54 +0600

    change password functionalities

--- Node 3/src/controllers/user.controller.js
+++ Node 3/src/controllers/user.controller.js
@@ -206,4 +206,22 @@ const refreshAccessToken = asyncHandler ( async (req, res) => {
     }
 })
 
-export {registerUser, loginUser, loggedOutUser, refreshAccessToken}
+const changeCurrentPassword = asyncHandler ( async (req, res) => {
+    const {oldPassword, newPassword} = req.body;
+    
+    const user = await User.findById(req.user?._id)
+    const isPasswordCorrect = await user.isPasswordCorrect(oldPassword)
+
+    if(!isPasswordCorrect){
+        throw new ApiError(400, "Invalid old password")
+    }
+
+    user.password = newPassword;
+    await user.save({validateBeforeSave: false})
+
+    res
+    .status(200)
+    .json(new ApiResponse(200, {}, "Password changed successfully"))
+})
+
+export {registerUser, loginUser, loggedOutUser, refreshAccessToken, changeCurrentPassword}
